27258677a7efc32e0a337144693e17a8954d8c59
MoYashaf <mohamedashabd55@gmail.com>
2025-07-05 14:36:47 +0300

    Added code for drawing arrows

--- src/Board.js
+++ src/Board.js
@@ -1,1 +1,242 @@
-export default class Board {}
+import { BOARD_DIM, boardState, pieceMap } from "./Chess.js";
+export default class Board {
+  constructor(uiBoard) {
+    this.uiBoard = uiBoard;
+    this.legalMoves = [];
+    this.selectedPiece = null;
+    this.draggedPieceElement = null;
+    this.arrowLayer = document.querySelector(".svg-layer");
+    this.svgArrows = [];
+
+    // Code for drawing arrows.
+    this.uiBoard.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      let targetSquare = e.target.closest(".square");
+      targetSquare.classList.toggle("highlighted-square");
+    });
+
+    this.uiBoard.addEventListener("mousedown", (e) => {
+      if (e.button != 2) return;
+
+      const square = e.target.closest(".square");
+      if (!square) return;
+      this.arrowStart = {
+        row: parseInt(square.dataset.row),
+        col: parseInt(square.dataset.col),
+      };
+    });
+
+    this.uiBoard.addEventListener("mouseup", (e) => {
+      if (e.button != 2 || !this.arrowStart) return;
+      const targetSquare = e.target.closest(".square");
+      if (!targetSquare) return;
+      const arrowEnd = {
+        row: parseInt(targetSquare.dataset.row),
+        col: parseInt(targetSquare.dataset.col),
+      };
+      if (
+        this.arrowStart.row == arrowEnd.row &&
+        this.arrowStart.col == arrowEnd.col
+      ) {
+        const arrowIndex = this.getSvgArrow(this.arrowStart, arrowEnd);
+        this.svgArrows[arrowIndex].remove();
+        this.svgArrows.splice(arrowIndex, 1);
+        return;
+      }
+
+      // Check if arrow already exists, in this case remove it.
+      const arrowExists = this.arrowExists(this.arrowStart, arrowEnd);
+      if (arrowExists) {
+        console.log("Found arrow");
+        let arrowIndex = this.getSvgArrow(this.arrowStart, arrowEnd);
+        console.log(arrowIndex);
+        console.log(this.svgArrows[arrowIndex]);
+        // this.svgArrows[arrowIndex].remove();
+        // this.svgArrows.splice(arrowIndex, 1);
+        return;
+      }
+      this.addSvgArrow(this.arrowStart, arrowEnd);
+      this.arrowStart = null;
+    });
+
+    for (let i = 0; i < BOARD_DIM; i++) {
+      for (let j = 0; j < BOARD_DIM; j++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.dataset.row = i;
+        square.dataset.col = j;
+        if ((i + j) % 2 == 0) {
+          square.classList.add("light-square");
+        } else {
+          square.classList.add("dark-square");
+        }
+        // This makes it so that all squares could be dragged on.
+        square.addEventListener("dragover", (e) => {
+          e.preventDefault();
+        });
+
+        square.addEventListener("drop", (e) => {
+          e.preventDefault();
+          const pieceRow = JSON.parse(
+            e.dataTransfer.getData("text/plain")
+          ).startRow;
+          const pieceCol = parseInt(
+            JSON.parse(e.dataTransfer.getData("text/plain")).startCol
+          );
+          let targetSquare = e.target.closest(".square");
+          const targetRow = parseInt(targetSquare.dataset.row);
+          const targetCol = parseInt(targetSquare.dataset.col);
+          const isLegalMove = this.legalMoves.some(
+            (move) => move[0] == targetRow && move[1] == targetCol
+          );
+          if (isLegalMove) {
+            targetSquare.innerHTML = "";
+            targetSquare.appendChild(this.draggedPieceElement);
+            // Perform the real move
+            const piece = boardState[pieceRow][pieceCol];
+            boardState[targetRow][targetCol] = piece;
+            boardState[pieceRow][pieceCol] = null;
+
+            this.selectedPiece = null;
+            this.legalMoves = [];
+            this.clearHighlights();
+            this.render();
+          }
+        });
+        this.uiBoard.appendChild(square);
+      }
+    }
+  }
+  render() {
+    const squares = document.querySelectorAll(".chess-board .square");
+    squares.forEach((square, index) => {
+      // Clear any exisiting piece.
+      square.innerHTML = "";
+      const row = Math.floor(index / BOARD_DIM);
+      const col = index % BOARD_DIM;
+      const piece = boardState[row][col];
+      if (piece) {
+        const pieceDiv = document.createElement("div");
+        pieceDiv.classList.add(
+          "piece",
+          `${piece.color}-${pieceMap[piece.type]}`
+        );
+        pieceDiv.setAttribute("draggable", "true");
+        pieceDiv.addEventListener("dragstart", (e) => {
+          this.clearArrows();
+          pieceDiv.classList.add("being-dragged");
+          this.selectedPiece = { row, col, piece };
+          this.legalMoves = this.selectedPiece.piece.getMoves(
+            this.selectedPiece.row,
+            this.selectedPiece.col,
+            boardState
+          );
+          this.highlightSquares(this.legalMoves);
+          this.draggedPieceElement = pieceDiv;
+          e.dataTransfer.setData(
+            "text/plain",
+            JSON.stringify({
+              startRow: this.selectedPiece.row,
+              startCol: this.selectedPiece.col,
+              pieceType: this.selectedPiece.piece.type,
+              pieceColor: this.selectedPiece.piece.color,
+            })
+          );
+          e.dataTransfer.effectAllowed = "move";
+        });
+        pieceDiv.addEventListener("dragend", (e) => {
+          e.target.classList.remove("being-dragged");
+          this.clearHighlights();
+          this.selectedPiece = null;
+          this.legalMoves = [];
+        });
+        square.appendChild(pieceDiv);
+      }
+    });
+  }
+  highlightSquares(squares) {
+    const squaresToBeHighlighted = new Array(squares.length);
+    for (let i = 0; i < squaresToBeHighlighted.length; i++) {
+      squaresToBeHighlighted[i] = document.querySelector(
+        `.square[data-row="${squares[i][0]}"][data-col="${squares[i][1]}"]`
+      );
+      squaresToBeHighlighted[i].classList.add("legal-move");
+    }
+  }
+  clearHighlights() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.classList.remove("legal-move");
+      square.classList.remove("highlighted-square");
+    });
+  }
+  getSquareCenter(row, col) {
+    const squareSize = this.uiBoard.clientWidth / BOARD_DIM;
+    return {
+      x: col * squareSize + squareSize / 2,
+      y: row * squareSize + squareSize / 2,
+    };
+  }
+  addSvgArrow(from, to) {
+    const svg = this.arrowLayer;
+    const fromPos = this.getSquareCenter(from.row, from.col);
+    const toPos = this.getSquareCenter(to.row, to.col);
+    const arrow = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "line"
+    );
+    arrow.setAttribute("x1", fromPos.x);
+    arrow.setAttribute("y1", fromPos.y);
+    arrow.setAttribute("x2", toPos.x);
+    arrow.setAttribute("y2", toPos.y);
+    arrow.setAttribute("stroke", "rgba(255, 0, 0, 0.6)");
+    arrow.setAttribute("stroke-width", "8");
+    arrow.setAttribute("marker-end", "url(#arrowhead)");
+    svg.appendChild(arrow);
+
+    this.svgArrows.push(arrow);
+  }
+  clearArrows() {
+    this.svgArrows.forEach((arrow) => {
+      console.log(arrow);
+      arrow.remove();
+    });
+    this.svgArrows = [];
+  }
+  arrowExists(arrowStart, arrowEnd) {
+    const arrowExist = this.svgArrows.some((arrow) => {
+      console.log(arrow.getAttribute("x1"));
+      console.log(this.getSquareCenter(arrowStart.row, arrowStart.col).x);
+      return (
+        arrow.getAttribute("x1") ==
+          this.getSquareCenter(arrowStart.row, arrowStart.col).x &&
+        arrow.getAttribute("x2") ==
+          this.getSquareCenter(arrowEnd.row, arrowEnd.col).x &&
+        arrow.getAttribute("y1") ==
+          this.getSquareCenter(arrowStart.row, arrowStart.col).y &&
+        arrow.getAttribute("y2") ==
+          this.getSquareCenter(arrowEnd.row, arrowEnd.col).y
+      );
+    });
+    return arrowExist;
+  }
+  getSvgArrow(arrowStart, arrowEnd) {
+    let index = null;
+    this.svgArrows.forEach((arrow, i) => {
+      if (
+        arrow.getAttribute("x1") ==
+          this.getSquareCenter(arrowStart.row, arrowStart.col).x &&
+        arrow.getAttribute("x2") ==
+          this.getSquareCenter(arrowEnd.row, arrowEnd.col).x &&
+        arrow.getAttribute("y1") ==
+          this.getSquareCenter(arrowStart.row, arrowStart.col).y &&
+        arrow.getAttribute("y2") ==
+          this.getSquareCenter(arrowEnd.row, arrowEnd.col).y
+      );
+      {
+        index = i;
+      }
+    });
+    return index;
+  }
+}
--- src/Chess.js
+++ src/Chess.js
@@ -1,3 +1,10 @@
+import Board from "./Board.js";
+import Bishop from "./pieces/Bishop.js";
+import Crocodile from "./pieces/Crocodile.js";
+import Knight from "./pieces/Knight.js";
+import Pawn from "./pieces/Pawn.js";
+import Rook from "./pieces/Rook.js";
+import Zebra from "./pieces/Zebra.js";
 export const BOARD_DIM = 14;
 
 export const COLORS = {
@@ -5,13 +12,319 @@ export const COLORS = {
   BLACK: "b",
 };
 
+// This board state represents the entire game board.
+// an empty square is 'null', and every other piece is an object.
+export let boardState = [
+  [
+    new Rook("b"),
+    null,
+    new Bishop("b"),
+    new Crocodile("b"),
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    new Zebra("b"),
+    new Bishop("b"),
+    null,
+    new Rook("b"),
+  ],
+  [
+    new Pawn("b"),
+    new Pawn("b"),
+    new Pawn("b"),
+    new Pawn("b"),
+    new Pawn("b"),
+    new Pawn("b"),
+    new Pawn("b"),
+    new Pawn("b"),
+    new Pawn("b"),
+    new Pawn("b"),
+    new Pawn("b"),
+    new Pawn("b"),
+    new Pawn("b"),
+    new Pawn("b"),
+  ],
+  [
+    null,
+    null,
+    new Knight("b"),
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    new Knight("b"),
+    null,
+    null,
+  ],
+  [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+  [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+  [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+  [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+  [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+  [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+  [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+  [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ],
+  [
+    null,
+    null,
+    new Knight("w"),
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    new Knight("w"),
+    null,
+    null,
+  ],
+  [
+    new Pawn("w"),
+    new Pawn("w"),
+    new Pawn("w"),
+    new Pawn("w"),
+    new Pawn("w"),
+    new Pawn("w"),
+    new Pawn("w"),
+    new Pawn("w"),
+    new Pawn("w"),
+    new Pawn("w"),
+    new Pawn("w"),
+    new Pawn("w"),
+    new Pawn("w"),
+    new Pawn("w"),
+  ],
+  [
+    new Rook("w"),
+    null,
+    new Bishop("w"),
+    new Crocodile("w"),
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    new Zebra("w"),
+    new Bishop("w"),
+    null,
+    new Rook("w"),
+  ],
+];
+// Used for assigning the correct CSS class for each piece.
+export const pieceMap = {
+  K: "king",
+  Q: "queen",
+  R: "rook",
+  B: "bishop",
+  N: "knight",
+  P: "pawn",
+  C: "crocodile",
+  Z: "crocodile",
+};
 export default class Chess {
-  constructor(uiBoard, currentTurn) {
+  constructor(uiBoard, currentTurn = "w") {
     this.uiBoard = uiBoard;
-    this.selectedPiece = null;
-    this.legalMoves = [];
-    this.currentTurn = this.currentTurn ? this.currentTurn : COLORS.WHITE;
+    this.currentTurn = currentTurn;
+    this.board = new Board(uiBoard);
   }
 
-  play() {}
+  play() {
+    this.uiBoard.addEventListener("click", (e) => {
+      /*
+  There's basically two outcomes.
+  1. You already selected a piece and need to move it.
+  2. You're just going to select a piece to check it's moves;
+  */
+      this.board.clearHighlights();
+      this.board.clearArrows();
+      const square = e.target.closest(".square");
+      if (!square) return;
+      const row = parseInt(square.dataset.row);
+      const col = parseInt(square.dataset.col);
+      if (this.board.selectedPiece) {
+        // I always forget what does the array method 'some' do so I'll explain it breifly
+        // It loops through the array and once it finds an element that meets the condition and returns true.
+        // In this case it sees if the selected move is a legal square to move to.
+        const isLegalMove = this.board.legalMoves.some(
+          (move) => move[0] == row && move[1] == col
+        );
+
+        if (isLegalMove) {
+          // ANIMATION START
+          let pieceToBeMoved = document.querySelector(
+            `.square[data-row="${this.board.selectedPiece.row}"][data-col="${this.board.selectedPiece.col}"] .piece`
+          );
+          pieceToBeMoved.style.transform = `translate(calc((${this.board.selectedPiece.col} - ${col}) * 50px * -1), calc((${this.board.selectedPiece.row} - ${row}) * 50px * -1));`;
+          this.board.clearHighlights();
+          pieceToBeMoved.addEventListener("transitionend", (_) => {
+            // We move the square by setting it's value to the destination and reseting its original square.
+            boardState[row][col] =
+              boardState[this.board.selectedPiece.row][
+                this.board.selectedPiece.col
+              ];
+            boardState[this.board.selectedPiece.row][
+              this.board.selectedPiece.col
+            ] = null;
+            // Reset everything.
+            this.board.selectedPiece = null;
+            this.board.legalMoves = [];
+            this.board.clearHighlights();
+            this.board.render();
+          });
+          // ANIMATION END
+        } else {
+          // In case the user plays an illegal move,
+          // or unselects the current piece.
+          this.board.selectedPiece = null;
+          this.board.legalMoves = [];
+          this.board.clearHighlights();
+          this.board.render();
+        }
+      } else {
+        // This means that the user is choosing a piece.
+        // We'll display its moves and highlight them.
+        this.board.selectedPiece = { row, col, piece: boardState[row][col] };
+        this.board.legalMoves = [
+          ...this.board.selectedPiece.piece.getMoves(
+            this.board.selectedPiece.row,
+            this.board.selectedPiece.col,
+            boardState
+          ),
+        ];
+
+        this.board.highlightSquares(this.board.legalMoves);
+      }
+    });
+    this.board.render();
+  }
 }
