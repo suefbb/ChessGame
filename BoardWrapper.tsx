6d07de1e0fac0d57d41b01d9402f62059307244f
MoYashaf <mohamedashabd55@gmail.com>
2025-08-29 17:06:09 +0300

    Got rid of deprecated code.

--- BoardWrapper.tsx
+++
@@ -1,5 +0,0 @@
-type IBoardWrapperProps = React.PropsWithChildren;
-
-export default function BoardWrapper({ children }: IBoardWrapperProps) {
-  return <div className="board-wrapper">{children}</div>;
-}
