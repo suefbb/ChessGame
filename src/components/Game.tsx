d4f9e97cb7cc4bb8c8d2a20ae2247d620257e2f8
suefbb <salwaelsaied2018@gmail.com>
2025-09-01 22:50:18 +0300

    Update Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -7,7 +7,8 @@ import {BlackPromotion} from "./Promotion";
 import { useGameState } from "../hooks/useGameState";
 import PGN from "./Controls/MovesList";
 import MovesControls from "./Controls/MovesControls";
-
+import { initialBoard } from "../core/types";
+import { useEffect } from "react";
 export default function Game() {
   const {
     board,
@@ -18,7 +19,10 @@ export default function Game() {
     resignClick,
     promotePawn,
     history,
+    isBPromotion,
+    isWPromotion,
     currentTurn,
+    result,
   } = useGameState();
   function clearLS(){
     localStorage.setItem('board' , JSON.stringify(initialBoard))
@@ -30,7 +34,6 @@ export default function Game() {
     localStorage.setItem('Wtm' , JSON.stringify([20 , 0 , 0 , 0]))
     localStorage.setItem('Btm' , JSON.stringify([20 , 0 , 0 , 0]))
   }
-
   return (
     <>
       <BoardWrapper>
@@ -55,8 +58,12 @@ export default function Game() {
           onBackwardClick={handleUndo}
           onForwardClick={handleRedo}
           onResignClick={resignClick}
+          turn={currentTurn}
         />
       </BoardWrapper>
+      <ResultDiv result={result} clearLocal={clearLS}/>
+      <WhitePromotion boarda={board} Pclick={promotePawn} showWhitePieces={isWPromotion}/>
+      <WhitePromotion boarda={board} Pclick={promotePawn} showBlackPieces={isBPromotion}/>
     </>
   );
 }
